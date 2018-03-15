7b22f8020cf2fa9db5bc22b809216f7aaaa15ab3
kanekot <kanekot>
2018-03-16 01:07:11 +0900

    add spring boot

---
+++ rest-client/src/main/java/com/sample/rest/SampleRestClient.java
@@ -0,0 +1,23 @@
+package com.sample.rest;
+
+public class SampleRestClient {
+
+	@SpringBootApplication
+	public class Application {
+	    public static void main(String[] args) {
+	        System.out.println("main()");
+	        try (ConfigurableApplicationContext ctx = SpringApplication.run(Application.class, args)) {
+	            Application app = ctx.getBean(Application.class);
+	            app.run(args);
+	        } catch (Exception e) {
+	            e.printStackTrace();
+	        }
+	    }
+	    public void run(String... args) throws Exception {
+	        System.out.println("処理開始");
+	        //アプリの処理
+	        System.out.println("処理終了");
+	    }
+	}
+
+}
