5dcbd2cdf6506c5913f9653a4d91ac568186bb20
kanekot <kanekot>
2018-03-16 02:20:43 +0900

    update

--- rest-client/src/main/java/com/sample/rest/SampleRestClient.java
+++ rest-client/src/main/java/com/sample/rest/SampleRestClient.java
@@ -1,24 +1,25 @@
 package com.sample.rest;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+
+@SpringBootApplication
 public class SampleRestClient {
 
-	@SpringBootApplication
-	public class Application {
-	    public static void main(String[] args) {
-	        System.out.println("main()");
-	        try (ConfigurableApplicationContext ctx = SpringApplication.run(Application.class, args)) {
-	            Application app = ctx.getBean(Application.class);
-	            app.run(args);
-	        } catch (Exception e) {
-	            e.printStackTrace();
-	        }
-	    }
-	    public void run(String... args) throws Exception {
-	        System.out.println("処理開始");
-	        //アプリの処理
-	        System.out.println("処理終了");
-	    }
+	public static void main(String[] args) {
+		System.out.println("main()");
+		try (ConfigurableApplicationContext ctx = SpringApplication.run(SampleRestClient.class, args)) {
+			SampleRestClient app = ctx.getBean(SampleRestClient.class);
+			app.run(args);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 	}
-
-	// a
+	public void run(String... args) throws Exception {
+		System.out.println("処理開始");
+		//アプリの処理
+		System.out.println("処理終了");
+	}
 }
+
